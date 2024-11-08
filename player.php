78eece29dd0b53a27da62a527e9acb5e42b89dc3
Michael Jival <michaeljival@gmail.com>
2024-11-08 15:54:11 -0600

    agregar controles 5 segundos y otros

--- player.php
+++ player.php
@@ -12,111 +12,164 @@ $videoId = $_GET['id'] ?? 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Player</title>
     <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
-    <meta name="csrf-token*" content="<?php echo $_SESSION['csrf_token']; ?>">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+    <!-- Estilos adicionales de Font Awesome -->
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css">
     <style>
-
-    .video {
-        width: 100% !important;
-        height: 100% !important;
-        /*background-color: green !important;*/
-        margin-top:0px !important;
-        position : absolute !important;
-    
-    }
-
-
-    
+        body {
+            margin: 0;
+            font-family: 'Poppins', sans-serif;
+            background-color: black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .video-container {
+            position: relative;
+            width: 100% !important;
+            height: 100% !important;
+            background-color: black;
+        }
+        .video {
+            width: 100% !important;
+            height: 100% !important;
+            margin-top: 0px !important;
+            position: absolute !important;
+        }
+        .controls {
+            position: absolute;
+            bottom: 38px !important;
+            left: 15%;
+            transform: translateX(-50%);
+            display: flex;
+            gap: 10px;
+            font-size: 12px;
+            color: white;
+            /*background-color: rgba(0, 0, 0, 0.5);*/
+            border-radius: 5px;
+            padding: 5px;
+        }
+        .controls button {
+            background: none;
+            border: none;
+            color: white;
+            cursor: pointer;
+            font-size: 18px;
+        }
     </style>
 </head>
 <body>
+    <div class="video-container">
+        <video class="video" id="video" controls></video>
+        <div class="controls">
+            <button title="Rebobinar 5 segundos" onclick="skipBack(10)"><i class="fa-sharp fa-regular fa-rotate-left"></i></button>
+            <button title="Adelantar 5 segundos" onclick="skipAhead(10)"><i class="fa-sharp fa-regular fa-rotate-right"></i></button></button>
+        </div>
+    </div>
 
-    <video class="video player" id="video" controls ></video>
+    <script>
+        const videoId = "<?php echo $videoId; ?>";
+        const videoElement = document.getElementById('video');
+        let halfwayNotified = false;
 
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    const videoId = "<?php echo $videoId; ?>";
-    const videoElement = document.getElementById('video');
-    let halfwayNotified = false; // Bandera para saber si ya se notificó el 50%
-
-    if (!videoId) {
-        console.error('ID de clase no definido');
-        return;
-    }
-
-    fetch(`https://drm.eweo.com/dashboard/processed_videos/${videoId}/${videoId}.json`)
-    .then(response => response.json())
-    .then(data => {
-        return fetch('api.php?action=decrypt', {
-            method: 'POST',
-            body: JSON.stringify({
-                content: data.encrypted,
-                iv: data.iv
-            }),
-            headers: {
-                'Content-Type': 'application/json'
+        document.addEventListener('DOMContentLoaded', function() {
+            if (!videoId) {
+                console.error('ID de clase no definido');
+                return;
             }
+
+            fetch(`https://drm.eweo.com/dashboard/processed_videos/${videoId}/${videoId}.json`)
+            .then(response => response.json())
+            .then(data => {
+                return fetch('api.php?action=decrypt', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        content: data.encrypted,
+                        iv: data.iv
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error al descifrar el contenido');
+                }
+                return response.text();
+            })
+            .then(decryptedContent => {
+                const videoSrc = URL.createObjectURL(new Blob([decryptedContent], { type: 'application/vnd.apple.mpegurl' }));
+                initializePlayer(videoSrc);
+            })
+            .catch(error => console.error('Error al obtener la fuente del video:', error));
         });
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Error al descifrar el contenido');
-        }
-        return response.text();
-    })
-    .then(decryptedContent => {
-        const videoSrc = URL.createObjectURL(new Blob([decryptedContent], { type: 'application/vnd.apple.mpegurl' }));
-        initializePlayer(videoSrc);
-    })
-    .catch(error => console.error('Error fetching video source:', error));
 
-    function initializePlayer(videoSrc) {
-        if (Hls.isSupported()) {
-            const hls = new Hls();
-            hls.loadSource(videoSrc);
-            hls.attachMedia(videoElement);
-            hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                videoElement.play().catch(e => console.error('Error al reproducir:', e));
+        function initializePlayer(videoSrc) {
+            if (Hls.isSupported()) {
+                const hls = new Hls();
+                hls.loadSource(videoSrc);
+                hls.attachMedia(videoElement);
+                hls.on(Hls.Events.MANIFEST_PARSED, () => {
+                    videoElement.currentTime = 2; // Establece el tiempo inicial en el segundo 2
+                    console.log('Video listo para reproducirse');
+                });
+            } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
+                videoElement.src = videoSrc;
+                videoElement.addEventListener('loadedmetadata', () => {
+                    console.log('Video listo para reproducirse');
+                });
+            } else {
+                console.error('HLS no es soportado en este navegador');
+            }
+
+            videoElement.addEventListener('timeupdate', function() {
+                const currentTime = videoElement.currentTime;
+                const duration = videoElement.duration;
+
+                if (!halfwayNotified && currentTime >= duration / 2) {
+                    halfwayNotified = true;
+                    window.parent.postMessage({
+                        type: 'videoHalfway',
+                        videoId: videoId
+                    }, '*');
+                }
             });
-        } else if (videoElement.canPlayType('application/vnd.apple.mpegurl')) {
-            videoElement.src = videoSrc;
-            videoElement.addEventListener('loadedmetadata', () => {
-                videoElement.play().catch(e => console.error('Error al reproducir:', e));
-            });
-        } else {
-            console.error('HLS no es soportado en este navegador');
-        }
 
-        // Añadir el listener para el evento 'timeupdate' 
-        videoElement.addEventListener('timeupdate', function() {
-            const currentTime = videoElement.currentTime;
-            const duration = videoElement.duration;
-
-            // Comprobar si se ha alcanzado el 50%
-            if (!halfwayNotified && currentTime >= duration / 2) {
-                halfwayNotified = true;
-                // Enviar un mensaje al parent window indicando que el video ha alcanzado el 50%
-                window.parent.postMessage({ 
-                    type: 'videoHalfway', 
-                    videoId: videoId 
+            videoElement.addEventListener('ended', function() {
+                window.parent.postMessage({
+                    type: 'videoEnded',
+                    videoId: videoId
                 }, '*');
+            });
+        }
+
+        function skipAhead(seconds) {
+            const newTime = videoElement.currentTime + seconds;
+            if (newTime <= videoElement.duration) {
+                videoElement.currentTime = newTime;
+            } else {
+                videoElement.currentTime = videoElement.duration;
             }
-        });
+        }
 
-        // Añadir el listener para el evento 'ended'
-        videoElement.addEventListener('ended', function() {
-            // Enviar un mensaje al parent window indicando que el video ha terminado
-            window.parent.postMessage({ 
-                type: 'videoEnded', 
-                videoId: videoId 
-            }, '*');
-        });
-    }
-});
-
-
-</script>
+        function skipBack(seconds) {
+            const newTime = videoElement.currentTime - seconds;
+            if (newTime >= 0) {
+                videoElement.currentTime = newTime;
+            } else {
+                videoElement.currentTime = 0;
+            }
+        }
+    </script>
 </body>
 </html>
